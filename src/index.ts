f56fd483a5aa05ad719b1875fad068e4ca89a306
Jack Heriz-Smith <jackherizsmith@gmail.com>
2021-04-12 13:13:57 +0100

    first

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import "reflect-metadata";
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
+import { buildSchema } from "type-graphql";
+import cors from "cors";
+import dotenv from "dotenv";
+import { ChatResolver } from './resolvers/chat';
+
+dotenv.config();
+
+const main = async () => {
+  const app = express();
+
+  app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [ChatResolver],
+      validate: false,
+    }),
+  });
+
+  apolloServer.applyMiddleware({
+    app,
+    cors: false,
+  });
+
+  app.listen(process.env.PORT, () => {
+    console.log(
+      `Server ready at http://localhost:${process.env.PORT}${apolloServer.graphqlPath}`
+    );
+  });
+};
+
+main().catch((err) => {
+  console.log(err);
+});
